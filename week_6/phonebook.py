500d9877e6a3a9aff0e4597518a0263915d7fddc
Sandy Pesic <sandy.pesic@gmail.com>
2026-02-16 11:33:53 -0800

    add week 6

---
+++ week_6/phonebook.py
@@ -0,0 +1,9 @@
+import csv
+
+name = input("Name: ")
+number = input("Number: ")
+
+with open("phonebook.csv", "a") as file:
+
+    writer = csv.DictWriter(file, fieldnames=["name", "number"])
+    writer.writerow({"name": name, "number": number})
